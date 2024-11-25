17cb55feb616d5ef6d89efa47eec63d13e5f20cf
Arnob Chowdhury <arnob11chowdhury@gmail.com>
2024-11-25 06:57:15 +0600

    added the functionality of bonus food

--- snake_game.cpp
+++ snake_game.cpp
@@ -8,8 +8,7 @@
 #define BLOCK_SIZE 20
 #define SNAKE_SPEED 5
 
-typedef struct SnakeSegment 
-{
+typedef struct SnakeSegment {
     int x, y;
 } SnakeSegment;
 
@@ -41,6 +40,12 @@ bool initializeSDL(SDL_Window** window, SDL_Renderer** renderer)
     return true;
 }
 
+void placeFood(int* x, int* y) 
+{
+    *x = (rand() % (SCREEN_WIDTH / BLOCK_SIZE)) * BLOCK_SIZE;
+    *y = (rand() % (SCREEN_HEIGHT / BLOCK_SIZE)) * BLOCK_SIZE;
+}
+
 int main(int argc, char* argv[]) 
 {
     SDL_Window* window = NULL;
@@ -57,7 +62,7 @@ int main(int argc, char* argv[])
     SDL_Event event;
 
     // Snake setup
-    SnakeSegment snake[100];
+    SnakeSegment snake[200];
     int snakeLength = 5;
     int snakeDirX = 1, snakeDirY = 0;
 
@@ -68,12 +73,18 @@ int main(int argc, char* argv[])
         snake[i].y = SCREEN_HEIGHT / 2;
     }
 
-    // Food setup
-    int foodX = (rand() % (SCREEN_WIDTH / BLOCK_SIZE)) * BLOCK_SIZE;
-    int foodY = (rand() % (SCREEN_HEIGHT / BLOCK_SIZE)) * BLOCK_SIZE;
+    // Regular food setup
+    int foodX, foodY;
+    placeFood(&foodX, &foodY);
+
+    // Bonus food setup
+    int bonusFoodX = -BLOCK_SIZE; // Start outside screen
+    int bonusFoodY = -BLOCK_SIZE;
+    bool bonusFoodActive = false;
 
     // Score setup
     int score = 0;
+    int foodsEaten = 0;
 
     // Timing variables
     Uint32 lastTime = SDL_GetTicks();
@@ -87,7 +98,6 @@ int main(int argc, char* argv[])
             {
                 running = false;
             } 
-
             else if (event.type == SDL_KEYDOWN) 
             {
                 switch (event.key.keysym.sym) 
@@ -115,45 +125,62 @@ int main(int argc, char* argv[])
                             snakeDirY = 0;
                         }
                         break;
+
                     case SDLK_RIGHT:
                         if (snakeDirX == 0) 
                         {
                             snakeDirX = 1;
                             snakeDirY = 0;
                         }
-
                         break;
                 }
             }
         }
 
-        // Game logic: For moving the snake
+        // Game logic: for moving the snake
         Uint32 currentTime = SDL_GetTicks();
         if (currentTime - lastTime > 100) 
         { 
-            // For controlling the snake speed
+            // for controlling the snake speed
             lastTime = currentTime;
 
-            // For moving snake body
+            // for moving snake body
             for (int i = snakeLength - 1; i > 0; --i) 
             {
                 snake[i] = snake[i - 1];
             }
 
-            // For moving snake head
+            // for moving snake head
             snake[0].x += snakeDirX * BLOCK_SIZE;
             snake[0].y += snakeDirY * BLOCK_SIZE;
 
-            // for checking collision with food
+            // for checking collision with regular food
             if (snake[0].x == foodX && snake[0].y == foodY) 
             {
                 score += 10;
                 snakeLength++;
-                foodX = (rand() % (SCREEN_WIDTH / BLOCK_SIZE)) * BLOCK_SIZE;
-                foodY = (rand() % (SCREEN_HEIGHT / BLOCK_SIZE)) * BLOCK_SIZE;
+                foodsEaten++;
+                placeFood(&foodX, &foodY);
+
+                // for activating bonus food after every 5 foods eaten
+                if (foodsEaten % 5 == 0) 
+                {
+                    placeFood(&bonusFoodX, &bonusFoodY);
+                    bonusFoodActive = true;
+                }
             }
 
-            // for checking collision with boundaries
+            // Check collision with bonus food
+            if (bonusFoodActive && snake[0].x == bonusFoodX && snake[0].y == bonusFoodY) 
+            {
+                score += 50; // Bonus score
+                snakeLength += 2; // Bonus growth
+                bonusFoodActive = false;
+                bonusFoodX = -BLOCK_SIZE;
+                bonusFoodY = -BLOCK_SIZE;
+            }
+
+            // Check collision with boundaries
             if (snake[0].x < 0 || snake[0].x >= SCREEN_WIDTH || snake[0].y < 0 || snake[0].y >= SCREEN_HEIGHT) 
             {
                 running = false;
@@ -164,49 +191,47 @@ int main(int argc, char* argv[])
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // Render food
+        // Render regular food
         SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
         SDL_Rect foodRect = {foodX, foodY, BLOCK_SIZE, BLOCK_SIZE};
         SDL_RenderFillRect(renderer, &foodRect);
+
+        // Render bonus food
+        if (bonusFoodActive) 
+        {
+            SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+            SDL_Rect bonusFoodRect = {bonusFoodX, bonusFoodY, BLOCK_SIZE, BLOCK_SIZE};
+            SDL_RenderFillRect(renderer, &bonusFoodRect);
+        }
 
         // Render snake
         SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
         for (int i = 0; i < snakeLength; ++i) 
         {
             SDL_Rect segmentRect = {snake[i].x, snake[i].y, BLOCK_SIZE, BLOCK_SIZE};
-
             SDL_RenderFillRect(renderer, &segmentRect);
         }
 
         // Render score
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
         char scoreText[32];
-
         sprintf(scoreText, "Score: %d", score);
-
         renderText(renderer, scoreText, 10, 10, (SDL_Color){255, 255, 255, 255});
 
         SDL_RenderPresent(renderer);
     }
 
     // Game over screen
-
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-
     SDL_RenderClear(renderer);
 
     renderText(renderer, "Game Over", SCREEN_WIDTH / 2 - 60, SCREEN_HEIGHT / 2 - 20, (SDL_Color){255, 0, 0, 255});
-
     char finalScore[32];
-
     sprintf(finalScore, "Final Score: %d", score);
-
     renderText(renderer, finalScore, SCREEN_WIDTH / 2 - 80, SCREEN_HEIGHT / 2 + 20, (SDL_Color){255, 255, 255, 255});
-
     SDL_RenderPresent(renderer);
 
-    SDL_Delay(3000);
+    SDL_Delay(3000); // Pause for 3 seconds
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
@@ -217,5 +242,5 @@ int main(int argc, char* argv[])
 
 void renderText(SDL_Renderer* renderer, const char* message, int x, int y, SDL_Color color) 
 {
-    
+
 }
